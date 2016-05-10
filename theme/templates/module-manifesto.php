8568b3db551bbddcc66f99f9abd5952a5739f876
Alexandre Segura <mex.zktk@gmail.com>
2016-05-10 18:28:55 +0200

    [TEMP] Modify manifesto.

--- theme/templates/module-manifesto.php
+++ theme/templates/module-manifesto.php
@@ -1,11 +1,13 @@
 <div id="manifesto" class="section">
-    <h2 class="section__title">Manifeste</h2>
+    <h2 class="section__title">Ensemble refusons le 49.3 !</h2>
     <div class="section__content">
-        <p><strong>Sais-­tu ce qui se passe là ? </strong>Des milliers de personnes se réunissent Place de la République à Paris, et dans toute la France, depuis le 31 mars. Des assemblées se forment où les gens discutent et échangent. Chacun se réapproprie la parole et l’espace public.</p>
-		<p>Ni entendues ni représentées, des personnes de tous horizons reprennent possession de la réflexion sur l’avenir de notre monde. La politique n’est pas une affaire de professionnels, <strong>c’est l’affaire de tous</strong>. L’humain devrait être au cœur des préoccupations de nos dirigeants. Les intérêts particuliers ont pris le pas sur l’intérêt général.</p>
-		<p><strong>Chaque jour, nous sommes des milliers à occuper l’espace public pour reprendre notre place dans la République.</strong>Venez nous rejoindre, et décidons ensemble de notre devenir commun.</p>
+        <p>Rassemblons nous tou-te-s ce soir <strong>à 18H pour un rassemblement devant l'Assemblée Nationale.</strong></p>
+		<p>1 307 000 signataires contre la loi travail, des centaines des milliers de manifestant-e-s à plusieurs reprises dans les rues, 200 #NuitDebout en France, 71% de la population contre la loi travail : la seule réponse du gouvernement c'est le passage en force et la répression !.</p>
+		<p>Après deux mois de manifestations massives contre le projet de "Loi Travail" et une opinion publique majoritairement opposée à ce texte, le gouvernement Hollande-Valls menace d'utiliser le 49.3 pour empêcher le débat à l'Assemblée nationale. Le rapporteur du projet, le député PS Christophe Sirugue, avoue qu'il manque 40 voix au parlement pour l'approuver. La majorité est donc divisée face à cette loi qui serait un recul sans précédent des droits des travailleuses et des travailleurs en France, un retour au XIXe siècle.</p>
+		<p>En mars, Manuel Valls menaçait de démissionner s'il était empêché d'appliquer la loi El Khomri. Aujourd'hui il brandit la menace du 49.3. Ce déni de démocratie après un mouvement social qui a regroupé des centaines de milliers de personnes dans la rue, dans les entreprises, les universités, les lycées et sur les places de France est une insulte au peuple de ce pays.</p>
+		<p>Plusieurs commissions de Nuit Debout Paris dénoncent cette volonté de passage en force. Si Manuel Valls persiste dans cette voie, <strong>nous répondrons par tous les moyens légitimes en proportion au mépris affiché</strong>. Nous appelons toutes et tous les démocrates à se mobiliser contre cette dernière attaque contre nos droits sociaux et notre démocratie.</p>
     </div>
     <div class="section__actions-container">
-        <a class="primary-button" href="http://petition.nuitdebout.fr/fr">Signer la pétition</a>
+        <a class="primary-button" href="https://www.facebook.com/events/1041846769235385/">Démocrates, debout !</a>
     </div>
 </div>
